9724edd53faa93fb9d41cbcfb94253d69c8a741a
Andrzej <andrzej.kieler@yahoo.com>
2020-10-13 19:32:41 +0200

    Working Note Entities with servlet made with jetty embedded in maven

--- src/main/java/operator/Operator.java
+++ src/main/java/operator/Operator.java
@@ -1,15 +1,37 @@
 package operator;
 
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Operator {
-    public static void main(String[] args){
-        System.out.println("Kel");
+    public static void main(String[] args) throws Exception {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/"); //aby wszystkie requesty zaczynały się od slasha
+    webapp.setConfigurations(new Configuration[]
+            {
+                    new AnnotationConfiguration(),
+                    new WebInfConfiguration(),
+                    new WebXmlConfiguration(),
+                    new MetaInfConfiguration(),
+                    new FragmentConfiguration(),
+                    new EnvConfiguration(),
+                    new PlusConfiguration(),
+                    new JettyWebXmlConfiguration()
+            });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
 
+        //webapp.addServlet(TimeAllocatorServlet.class, "/api/*")
+        var server = new Server(8080);
+        server.setHandler(webapp);
 
-        Logger logger = LoggerFactory.getLogger(Operator.class);
-        logger.info("HelloWorld");
+        server.start();
+        server.join();
     }
 }
