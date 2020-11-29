abd4702e1db0161f6d0a6a225a25bbcfdf4ce626
Andrzej <andrzej.kieler@yahoo.com>
2020-11-29 18:16:14 +0100

    SpringBoot dummy 2

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -13,6 +13,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Date date = new Date();
         Logger logger = LoggerFactory.getLogger(App.class);
-
+        SpringApplication.run(App.class, args);
     }
 }
